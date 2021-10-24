470fc2f720856416cc0d8fe36b543c5c8518ede8
Kirill Titov <work@kirilltitov.com>
2021-10-24 16:40:24 +0300

    Drop swift-log dependency

--- Sources/Entita2/Entita2.swift
+++ Sources/Entita2/Entita2.swift
@@ -1,11 +1,6 @@
-import Logging
-
 public enum Entita2 {
     /// Global default serializing format for all Entita2 entities
     public static var format: Entita2.Format = .JSON
-
-    /// Global logger
-    public static var logger: Logger = Logger(label: "Entita2")
 }
 
 public typealias E2 = Entita2
